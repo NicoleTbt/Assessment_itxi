2d3b199996c983474643ae537f7ad084a4945b2c
NicoleTbt <124452338+NicoleTbt@users.noreply.github.com>
2025-06-04 21:53:24 +0300

    Develop the splash screen. Show the splash screen for 3 seconds before rendering the navigation stack.

--- App.tsx
+++ App.tsx
@@ -1,17 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import {StatusBar, StyleSheet, useColorScheme} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {StatusBar, StyleSheet, useColorScheme, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {RootStackNavigator} from './src/app/Presentation/Navigation/RootStack';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {RootStackNavigator} from './src/app/Presentation/Navigation/RootStack';
+import SplashScreen from './src/app/Presentation/Screens/Shared/SplashScreen';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [showSplash, setShowSplash] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return <SplashScreen />;
+  }
 
   return (
     <GestureHandlerRootView style={styles.gestureView}>
       <NavigationContainer>
-        <SafeAreaView style={styles.safeArea}>
+        <SafeAreaView style={styles.safeArea} edges={['top', 'bottom']}>
           <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
           <RootStackNavigator />
         </SafeAreaView>
@@ -27,6 +41,7 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     padding: 5,
+    backgroundColor: '#ffff',
   },
 });
 
--- src/app/Presentation/Screens/Shared/SplashScreen.tsx
+++ src/app/Presentation/Screens/Shared/SplashScreen.tsx
@@ -1,12 +1,20 @@
-import {View, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, ActivityIndicator} from 'react-native';
 import React from 'react';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <ActivityIndicator size="large" />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default SplashScreen;
